4760039fb86a3102b364ad1b2f893aaa1bb8c87d
Juan <juanvj0425@gmail.com>
2023-05-10 20:40:34 -0500

    Deleting unused folder

--- Desktop/Projects/makeitreal/mision 4/js-1/hello.js
+++
@@ -1,7 +0,0 @@
-function hola(name) {
-    return("Hola " + name + "!");
-  }
-
-// console.log(hola("Pedro")) // "Hola Pedro!"
-// console.log(hola("Juan")) // "Hola Juan!"
-// console.log(hola("")) // "Hola !"
